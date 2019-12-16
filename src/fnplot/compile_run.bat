26267d8f7cb9604a63d7972e6b03eaf83a8b9af0
dean morgan <deanmorgan@deans-MacBook-Air.local>
2019-12-16 17:24:06 -0500

    dean

--- src/fnplot/compile_run.bat
+++ src/fnplot/compile_run.bat
@@ -3,7 +3,7 @@ title Compiles and runs java files
 
 rem this is an explanation
 
-javac -d bin gui\*.java semantics\*java syntax\*.java sys\*java values\*.java -cp ".;syntax\java-cup-11b-runtime.jar;gui\cs34q-utils.jar"
+javac -d bin gui/*.java semantics/*.java syntax/*.java sys/*java values/*.java syntax/inbuiltfunctions/*.java -cp ".:syntax/java-cup-11b-runtime.jar:gui/cs34q-utils.jar"
 
-java -cp "bin;gui\cs34q-utils.jar;syntax\java-cup-11b-runtime.jar" fnplot.gui.FnPlotFrame
+java -cp "bin:gui/cs34q-utils.jar:syntax/java-cup-11b-runtime.jar" fnplot.gui.FnPlotFrame
 
